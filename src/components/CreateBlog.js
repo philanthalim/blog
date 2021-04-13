57e857d65abcce54a19877e90c67d2c8b0131a16
Phiju <PhijuFile@phijus-air.lan>
2021-04-13 20:25:25 +0800

    changed

--- src/components/CreateBlog.js
+++ src/components/CreateBlog.js
@@ -1,23 +1,23 @@
 import React from "react";
-import { useState} from "react";
+import { useState } from "react";
 import Axios from "axios";
 import { Link } from "react-router-dom";
 
 const Createblog = () => {
   const [title, setTitle] = useState("");
   const [story, setStory] = useState("");
+  const [image,setImage]=useState("https://images.pexels.com/photos/3910065/pexels-photo-3910065.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")
 
   const submitBlog = () => {
     Axios.post("https://blogzspot.herokuapp.com/api/blogs", {
       title: title,
       story: story,
+      image:image
     });
   };
   return (
     <div className="App">
-      <div
-        style={{ color: "null", padding: "20px", backgroundColor: "null" }}
-      >
+      <div style={{ color: "null", padding: "20px", backgroundColor: "null" }}>
         <h1>Create a Blog Post</h1>
       </div>
       <div
@@ -41,6 +41,7 @@ const Createblog = () => {
             className="story-input"
             onChange={(e) => setStory(e.target.value)}
           ></textarea>
+          <input type="file" name="photo" accept='image/*' onChange={(e)=>setImage(e.target.files[0].name)} ></input>
           <Link to="/blogs">
             <button className="post-button" onClick={submitBlog}>
               Create
